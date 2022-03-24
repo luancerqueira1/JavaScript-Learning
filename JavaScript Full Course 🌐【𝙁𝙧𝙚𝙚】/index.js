18dd62e4cce4baa900ca556fbe1c4753e2e90749
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-03-23 21:12:51 -0300

    done for today

--- JavaScript Full Course 🌐【𝙁𝙧𝙚𝙚】/index.js
+++ JavaScript Full Course 🌐【𝙁𝙧𝙚𝙚】/index.js
@@ -63,13 +63,82 @@ console.log(result);
 
 // let username = window.prompt('whars your name?');
 // console.log(username);
-
+/*
 let username;
 
 document.getElementById('myButton').onclick = function (){
 
     username = document.getElementById('myText').value;
-    console.log(username);
+    console.log(username)
     document.getElementById('myLabel').innerHTML = 'hello ' + username;
-
 }
+*/
+
+/*======================= AULA 4 =======================*/
+
+/*
+let age = window.prompt("How old are you?");
+
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+
+console.log("Happy Birthday! You are", age, "years old");
+
+
+let x;
+let y;
+let z;
+
+x = Number(3.14);
+y = String(3.14);
+z = Boolean('pizza');
+
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
+*/
+
+
+/*======================= AULA 5 =======================*/
+
+// const = a variable that can't be changed
+
+/*
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+// PI = 420.69;
+
+circumference = 2 * PI * radius;
+console.log("The circumference is:", circumference);
+*/
+
+/*======================= AULA 6 =======================*/
+
+/*
+let x = 3.14;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+//x Math.round (x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x Math.pow(x, 2);
+//x Math.sqrt(x);
+//x = Math.abs (x);
+
+maximum = Math.max(x, y, z);
+minimum = Math.min(x, y, z);
+
+x = Math.round(x);
+
+console.log(maximum)
+*/
