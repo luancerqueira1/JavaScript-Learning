23d84d3448b2ee15f1bc86760073fbdcec959f56
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-03 19:17:09 -0300

    dgh csaugfwvecduwjfehsdvujc

--- JavaScript Full Course 🌐【𝙁𝙧𝙚𝙚】/index.js
+++ JavaScript Full Course 🌐【𝙁𝙧𝙚𝙚】/index.js
@@ -2127,124 +2127,261 @@ document.getElementById('submitButton').onclick = function () {
 // }
 
 /*====================== AULA 83 ======================*/
+//JOGO DA VELHA
 
-const cells = document.querySelectorAll(".cell")
-const statusText = document.querySelector("#statusText");
-const restartBtn = document.querySelector("#restartBtn");
-const winconditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+// const cells = document.querySelectorAll(".cell")
+// const statusText = document.querySelector("#statusText");
+// const restartBtn = document.querySelector("#restartBtn");
+// const winconditions = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ]
 
-let options = ['', '', '', '', '', '', '', '', '']
-let currentPlayer = 'x'
-let running = false
+// let options = ['', '', '', '', '', '', '', '', '']
+// let currentPlayer = 'x'
+// let running = false
 
-initializeGame()
+// initializeGame()
 
-function initializeGame() {
-  cells.forEach(cell => cell.addEventListener('click', cellClicked))
-  restartBtn.addEventListener('click', restartGame)
-  statusText.textContent = `${currentPlayer}'s turn`;
-  running=  true
-}
+// function initializeGame() {
+//   cells.forEach(cell => cell.addEventListener('click', cellClicked))
+//   restartBtn.addEventListener('click', restartGame)
+//   statusText.textContent = `${currentPlayer}'s turn`;
+//   running=  true
+// }
 
-function cellClicked() {
-  const cellIndex = this.getAttribute('cellIndex')
+// function cellClicked() {
+//   const cellIndex = this.getAttribute('cellIndex')
 
-  if (options[cellIndex] != '' || !running) {
-    return
-  }
-  updateCell(this, cellIndex)
-  checkWinner()
-}
+//   if (options[cellIndex] != '' || !running) {
+//     return
+//   }
+//   updateCell(this, cellIndex)
+//   checkWinner()
+// }
 
-function updateCell(cell, index) {
-  options[index] = currentPlayer
-  cell.textContent =  currentPlayer
-}
+// function updateCell(cell, index) {
+//   options[index] = currentPlayer
+//   cell.textContent =  currentPlayer
+// }
 
-function changePlayer() {
-  currentPlayer = (currentPlayer == "x") ? "o" : "x"
-  statusText.textContent = `${currentPlayer}'s turn`
-}
+// function changePlayer() {
+//   currentPlayer = (currentPlayer == "x") ? "o" : "x"
+//   statusText.textContent = `${currentPlayer}'s turn`
+// }
 
-function checkWinner() {
-  let roundWon = false
+// function checkWinner() {
+//   let roundWon = false
 
-  for (let i = 0; i < winconditions.length; i++){
-    const condition = winconditions[i];
-    const cellA = options[condition[0]];
-    const cellB = options[condition[1]];
-    const cellC = options[condition[2]];
+//   for (let i = 0; i < winconditions.length; i++){
+//     const condition = winconditions[i];
+//     const cellA = options[condition[0]];
+//     const cellB = options[condition[1]];
+//     const cellC = options[condition[2]];
 
-    if (cellA == '' || cellB == '' || cellC == '') {
-      continue
-    }
-    if (cellA == cellB && cellB == cellC) {
-      roundWon = true
-      break
-    }
-  }
-  if (roundWon) {
-    statusText.textContent = `${currentPlayer} wins`
-    running = false
-  }
-  else if (!options.includes('')) {
-    statusText.textContent = `draw`;
-    running = false
-  }
-  else {
-    changePlayer()
-  }
-}
-
-
-function restartGame() {
-  currentPlayer = 'x'
-  options = ["", "", "", "", "", "", "", "", ""]
-  statusText.textContent = `${currentPlayer}'s turn`
-  cells.forEach(cell => cell.textContent = '')
-  running = true
-
-}
+//     if (cellA == '' || cellB == '' || cellC == '') {
+//       continue
+//     }
+//     if (cellA == cellB && cellB == cellC) {
+//       roundWon = true
+//       break
+//     }
+//   }
+//   if (roundWon) {
+//     statusText.textContent = `${currentPlayer} wins`
+//     running = false
+//   }
+//   else if (!options.includes('')) {
+//     statusText.textContent = `draw`;
+//     running = false
+//   }
+//   else {
+//     changePlayer()
+//   }
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function restartGame() {
+//   currentPlayer = 'x'
+//   options = ["", "", "", "", "", "", "", "", ""]
+//   statusText.textContent = `${currentPlayer}'s turn`
+//   cells.forEach(cell => cell.textContent = '')
+//   running = true
+// }
 
 /*====================== AULA 84 ======================*/
+
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext('2d')
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.Height;
+const boardBackground = "white"
+const snakeColor = 'lightgreen'
+const snakeBorder = 'black'
+const foodColor = 'red'
+const unitSize = 25
+let running = false
+let xVelocity = unitSize
+let yVelocity = 0
+let foodX
+let foodY
+let score = 0
+let snake = [
+  {x:unitSize * 4, y:0},
+  {x:unitSize * 3, y:0},
+  {x:unitSize * 2, y:0},
+  {x:unitSize, y:0},
+  {x:0, y:0}
+]
+
+window.addEventListener('keydown', changeDirection)
+resetBtn.addEventListener('click', resetGame)
+
+gameStart()
+
+
+function gameStart() { 
+  running = true
+  scoreText.textContent = score
+  createFood()
+  drawFood()
+  nextTick()
+};
+
+function nextTick() { 
+  if (running) {
+    setTimeout(() => {
+      clearBoard()
+      drawFood()
+      moveSnake()
+      drawSnake()
+      checkGameOver()
+      nextTick()
+    }, 75)
+  }
+  else {
+    displayGameOver()
+  }
+};
+
+function clearBoard() { 
+  ctx.fillStyle = boardBackground
+  ctx.fillRect(0,0 ,gameWidth, gameHeight)
+};  
+
+function createFood() {
+  function randomFood(min , max) {
+    const randNum = Math.round((Math.random() * (max - min) + min)/ unitSize) * unitSize;
+    return randNum;
+  }
+  foodX = randomFood(0, gameWidth - unitSize)
+  foodY = randomFood(0, gameWidth - unitSize)
+
+}
+ 
+
+function drawFood() { 
+  ctx.fillStyle = foodColor
+  ctx.fillRect(foodX, foodY,unitSize,unitSize)
+};
+
+function moveSnake() { 
+  const head = {
+    x: snake[0].x + xVelocity,
+    y: snake[0].y + yVelocity
+  };
+  snake.unshift(head)
+  //if food is eaten
+  if (false) {
+    
+  }
+  
+  else {
+    snake.pop()
+  }
+};
+
+function drawSnake() { 
+  ctx.fillStyle = snakeColor
+  ctx.strokeStyle = snakeBorder
+  snake.forEach(snakePart => {
+    ctx.fillRect(snakePart.x , snakePart.y, unitSize, unitSize)
+    ctx.strokeRect(snakePart.x , snakePart.y, unitSize, unitSize)
+  })
+};
+
+function changeDirection(event) { 
+  const keyPressed = event.keyCode
+  const LEFT = 37
+  const UP = 38
+  const RIGHT = 39
+  const DOWN = 40
+
+  const goingUp = (yVelocity == -unitSize)
+  const goingDown = (yVelocity == unitSize)
+  const goingRight = (xVelocity == unitSize)
+  const goingLeft = (xVelocity == -unitSize)
+
+  switch (true) {
+    case keyPressed == LEFT && !goingRight:
+      xVelocity = -unitSize;
+      yVelocity = 0;
+      break;
+    case keyPressed == UP && !goingDown:
+      xVelocity = 0;
+      yVelocity = -unitSize;
+      break;
+    case keyPressed == RIGHT && !goingLeft:
+      xVelocity = unitSize;
+      yVelocity = 0;
+      break;
+    case keyPressed == DOWN && !goingUp:
+      xVelocity = 0;
+      yVelocity = unitSize;
+      break;
+  }
+};
+
+function checkGameOver() { };
+
+function displayGameOver() { };
+
+function resetGame (){};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*====================== AULA 85 ======================*/
 /*====================== AULA 86 ======================*/
 /*====================== AULA 87 ======================*/
