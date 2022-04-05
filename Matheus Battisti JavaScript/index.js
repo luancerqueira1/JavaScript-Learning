c46ca08a668b1ef02e5a2a3a92382a8ca96ca337
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-05 17:45:01 -0300

    aula 8

---
+++ Matheus Battisti JavaScript/index.js
@@ -0,0 +1,27 @@
+
+/*=============== Aula 6 ===============*/
+
+// let numero = 5;
+// console.log(typeof numero)
+
+// let float = 5.6
+// console.log(typeof float)
+
+// let textoComNumero = '455'
+// console.log(typeof textoComNumero)
+
+// let soma = float + textoComNumero
+// console.log(soma)
+
+
+/*=============== Aula 7 ===============*/
+
+// let nome = 'luan';
+// let sobrenome = 'gama';
+
+// let nomeCompleto = nome + ' ' + sobrenome;
+// console.log(nomeCompleto)
+
+
+/*=============== Aula 8 ===============*/
+// boolean = true or false
