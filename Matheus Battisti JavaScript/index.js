998bdf7422e21f1fb3a55a3ebc7a1917a9306c33
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-06 12:42:43 -0300

    video 20 done

--- Matheus Battisti JavaScript/index.js
+++ Matheus Battisti JavaScript/index.js
@@ -1,5 +1,5 @@
 
-/*=============== Aula 6 ===============*/
+/*=============== Video 6 ===============*/
 
 // let numero = 5;
 // console.log(typeof numero)
@@ -14,7 +14,7 @@
 // console.log(soma)
 
 
-/*=============== Aula 7 ===============*/
+/*=============== Video 7 ===============*/
 
 // let nome = 'luan';
 // let sobrenome = 'gama';
@@ -23,5 +23,170 @@
 // console.log(nomeCompleto)
 
 
-/*=============== Aula 8 ===============*/
+/*=============== Video 8 ===============*/
 // boolean = true or false
+
+
+/*=============== Video 10 ===============*/
+//OBJETOS
+
+// let obj = {
+//     nome: 'matheus ',
+//     idade: 29,
+//     profissao: 'programador',
+//     trabalhando: true,
+// };
+
+// console.log( obj)
+// console.log(typeof obj)
+
+// //acessar o objeto
+// console.log(obj.nome);
+
+// console.log(obj.idade)
+// console.log(obj.profissao)
+
+// console.log('0 meu nome é' + ' ' + obj.nome)
+
+// obj.graduacao = true
+// console.log(obj)
+
+
+/*=============== Video 11 ===============*/
+
+// let arr = [5, 'luan', true,10]
+
+// console.log(arr)
+// console.count(arr[3])
+
+
+/*=============== Video 12 ===============*/
+
+// == IGUALDADE
+// != DIFERENTE
+
+// let idade = 18
+// let idadeMinima = 18
+
+// if (idade >= idadeMinima) {
+//     console.log('pode dirigir')
+// }
+// else {
+//     console.log('nao pode dirigir')
+// }
+
+
+/*=============== Video 14 ===============*/
+
+// let nome = 'luan'
+
+// if (nome == 'luan') {
+//     console.log('o meu nome é luan')
+// } else if(true){
+//     console.log('meu nome nao é luan')
+// }
+
+
+/*=============== Video 15 ===============*/
+
+// let numero = 51
+
+// if (numero === 5) {
+//     console.log(`o numero é: ${numero}`)
+// }
+
+// if (numero !== 5) {
+//     console.log(`o numero nao é: ${numero}`)
+// }
+
+/*=============== Video 16 && 17 && 18 ===============*/
+
+//OPARADORES LÓGICOS =  "&&""    "||"     "!""
+
+// let idade = 24
+// let nome = 'luan'
+
+// if (idade == 24 && nome == 'luan') {
+//     console.log('vai passar de ano')
+// }
+// else if (3 > 1){
+//     console.log('vai perder fila da puta')
+// }
+// else  if((1 == 1 && 4 > 3) || nome == 'luan') {
+//   console.log("Passou");
+// }
+// else if (nome === 'luan' && idade >= 14) {
+//   console.log("Aqui passa!");
+// }
+
+// if (!true) {
+//     console.log('true')
+// }
+
+// let meunome = 'luan'
+
+// if (!(meunome == 'luan')) {
+//     console.log('ok')
+// }
+// else {
+//     console.log('express')
+// }
+
+
+/*=============== Video 19 ===============*/
+
+// let x = 0
+
+// while (x < 5) {
+    
+//     console.log('textando repeticao ' + x)
+    
+//     //incrementador
+//     x++;
+// }
+
+// let arr = ['teste', 'textando ', ' a', 'b']
+// let y = 0
+
+// while (y <= 3) {
+//     console.log(arr[y])
+
+//     y++
+// }
+
+// var palavra = "Matheus"
+// var i = 0;
+
+// while (i <= 6) {
+//     console.log(palavra[i]);
+
+//     i++
+// }
+
+
+/*=============== Video 20 ===============*/
+
+// let x = 1
+// let y = 2
+
+// console.log(x = x + y)
+// console.log(x += y)
+
+// console.log(x -= y)
+
+// console.log(x *= y)
+
+// console.log(x /= y)
+
+// //loops
+// console.log(x++);
+// console.log(x--);
+
+// while (x <= 100) {
+//     console.log(x)
+
+//     x*=2
+// }
+
+
+ 
