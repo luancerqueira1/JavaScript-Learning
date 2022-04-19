51b5402ca8b8bf230f5ec8a1052d11d0894dd367
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-19 15:47:07 -0300

    1

---
+++ JavaScriptProgramming/counter people/index.js
@@ -0,0 +1,60 @@
+
+// document.getElementById("count-el").innerText = 5 ;
+
+// let count = 7
+// console.log(count)
+
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log (count)
+
+/*📌==========================================================📌*/
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log my DogAge to the console
+
+// let myAge = 24
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+/*📌========================================================= 📌*/
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+/*📌========================================================= 📌*/
+ // Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+// function myLogger() {
+// console.log(42)
+// }
+// myLogger ()
+
+/*📌========================================================= 📌*/
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+ 
+//  // Create a function that logs out the sum of all the lap times
+// function logLapTime() {
+//    // let totalTime = lap1 + lap2 + lap3
+//    console.log(lap1 + lap2 + lap3);
+// }
+// logLapTime();
+
+/*📌==========================================================📌*/
+function increment() {
+	console.log('button clickled')
+}
