796b890b8abb1c64cccc16c673556ce1c75f8d36
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-21 14:47:16 -0300

    sanuel me atrapalhou

--- JavaScriptProgramming/calculator/calculator.js
+++ JavaScriptProgramming/calculator/calculator.js
@@ -36,17 +36,49 @@
 // 	document.getElementById("sum-el").textContent = "SUM: " + sum;
 // });
 
+/*📌==========================================================📌*/
+// let firstCard = 10
+// let secondCard = 17
+// let sum = firstCard + secondCard
+
+// if (sum < 21) {
+// 	console.log("do you want a new card?")
+// } else if (sum === 21) {
+// 	console.log("you´ve got blackjack!")
+// } else {
+// 	console.log("you lost")
+// }
 
 /*📌==========================================================📌*/
-let firstCard = 10
-let secondCard = 17
-let sum = firstCard + secondCard 
+ // Check if the person is elegible for a birthday card from the King! (100)
+// if less than 100 -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday Card from the King!"
+// else-> "Not elegible, you have already gotten one"
+// let age = 101;
 
-if (sum < 21) {
-	console.log("do you want a new card?")
+// if (age < 100) {
+// 	console.log("not elegible")
+// } else if (age === 100) {
+// 	console.log("Here is your birthday Card from the King!");
+// } else {
+// 	console.log("Not elegible, you have already gotten one");
+// }
+
+/*📌==========================================================📌*/
+//Write the conditional according to these rules:
+// if less than or equal to 20 -> "Do you want to draw a new card?
+// else if exactly 21 -> "Wohoo! You've got Blackjack!
+// else -> "You're out of the game!
+
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard + 4;
+
+if (sum <= 20) {
+	console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
-	console.log("you´ve got blackjack!")
+	console.log("Wohoo! You've got Blackjack!");
 } else {
-	console.log("you lost")
-} 
+	console.log("You're out of the game!");
+}
 
