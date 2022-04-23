0b071a2d6e10f5808d7eaf2185897a30fb2af62d
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-22 22:30:51 -0300

    done  for today

--- JavaScriptProgramming/blackjack/blackjack.js
+++ JavaScriptProgramming/blackjack/blackjack.js
@@ -81,14 +81,22 @@
 
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
 
 function startGame() {
+	renderGame()
+}
+
+function renderGame() {
+	cardsEl.textContent = "cards: " + cards[0]+ " " + cards[1]
 	sumEl.textContent = "sum: " + sum
 	if(sum <= 20) {
 		message = "Do you want to draw a new card?";
@@ -100,6 +108,13 @@ function startGame() {
 		isAlive = false
 	}
 	messageEl.textContent = message
+}
+
+function newCard() {
+	let card = 10
+	sum += card
+
+	renderGame()
 }
 
 
