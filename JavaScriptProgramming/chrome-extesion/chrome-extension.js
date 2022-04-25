3a1d7360679dbf3e2ccf01e5b30e2cd14ddfc137
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-25 13:14:41 -0300

    pausa para o almoçco

---
+++ JavaScriptProgramming/chrome-extesion/chrome-extension.js
@@ -0,0 +1,16 @@
+// let openBox = document.getElementById("box");
+
+// openBox.addEventListener("click", function () {
+//   console.log("i want open the box");
+// });
+
+/*📌==========================================================📌*/
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+let saveBtn = document.getElementById("input-btn");
+
+
+
+saveBtn.addEventListener("click", function () {
+	console.log("add  from adventner listener")
+})
