753cbfe952732c097bd5ebddf45fd6e0e4d374d8
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-26 13:57:02 -0300

    termino mais tarde

--- JavaScriptProgramming/chrome-extesion/chrome-extension.js
+++ JavaScriptProgramming/chrome-extesion/chrome-extension.js
@@ -31,39 +31,81 @@
 // }
 
 /*📌==========================================================📌*/
-// template strings/literals
-const recipient = "James"
-let sender = "luan"
-// Refactor the email string to use template strings
-const email = 
-`Hey ${recipient}!
-How is it going? 
-Cheers ${sender}`;
-console.log (email)
-
-
+// // template strings/literals
+// const recipient = "James"
+// let sender = "luan"
+// // Refactor the email string to use template strings
+// const email =
+// `Hey ${recipient}!
+// How is it going?
+// Cheers ${sender}`;
+// console.log (email)
+ 
 /*📌==========================================================📌*/
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-let saveBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+// let myLeads = []
+// const inputEl  =document.getElementById("input-el")
+// const inputBtn = document.getElementById ("input-btn")
+// const ulEl = document.getElementById ("ul-el")
 
-saveBtn.addEventListener("click", function () {
-	myLeads.push(inputEl.value)
-	renderLeads()
-	inputEl.value = ""
-});
+// // 1. Save a key-value pair in localStorage
+// // 2. Refresh the page. Get the value and log it to the console
+// // 3. Clear localStorage
 
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-   //listItems +="<li> <a href= '" + myLeads[i] + "' target='_blank'> " + myLeads[i] + "</a> </li>";
-	listItems +=
-	`<li> 
-		<a href= '${myLeads[i]}' target='_blank'>  ${myLeads[i]} <a>
-	</li>`;
+// // HINTS
+// // localStorage.setItem(key, value)
+// //localStorage.getItem(key)
+// // localStorage.clear()
+// // PS: both key and value need to be strings
 
-  }
-  ulEl.innerHTML = listItems;
-}
+// localStorage.setItem("myName", "Luan")
+// console.log(localStorage)
+// let myname = localStorage.getItem("myName")
+// console.log(myname)
+// localStorage.clear()
+
+// /*📌==========================================================📌*/
+let myLeads = `["www.awesomelead.com"]`
+ console.log(myLeads);
+// 1. Turn the myLeads string into an array
+myLeads = JSON.parse(myLeads);
+console.log(myLeads);
+// 2. Push a new value to the array
+myLeads.push("www.luan.com");
+console.log(myLeads);
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads);
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
+
+
+
+// /*📌==========================================================📌*/
+// let myLeads = ["www.google.com"];
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof myLeads)
+
+// const inputEl = document.getElementById("input-el");
+// let saveBtn = document.getElementById("input-btn");
+// const ulEl = document.getElementById("ul-el");
+
+// localStorage.setItem("myLeads", "www.google.com" )
+
+// saveBtn.addEventListener("click", function () {
+// 	myLeads.push(inputEl.value)
+// 	inputEl.value = ""
+// 	renderLeads()
+// });
+
+// function renderLeads() {
+//   let listItems = "";
+//   for (let i = 0; i < myLeads.length; i++) {
+//    //listItems +="<li> <a href= '" + myLeads[i] + "' target='_blank'> " + myLeads[i] + "</a> </li>";
+// 	listItems +=
+// 	`<li> 
+// 		<a href= '${myLeads[i]}' target='_blank'>  ${myLeads[i]} <a>
+// 	</li>`;
+
+//   }
+//   ulEl.innerHTML = listItems;
+// }
 
