891eb66f27b6bcf34f3e9fabbc62cf44ff3ccf8f
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-04-28 22:24:17 -0300

    nao consegui deixa pra amanha

--- TraversyMedia/main.js
+++ TraversyMedia/main.js
@@ -50,40 +50,114 @@
 // console.log(person.email);
 
 					//	⬇️ Arrays de objetos ⬇️
-const todos = [
-	{
-		id: 1,
-		text: "take out the trash",
-		isCompleted: true
-	},
-	{
-		id: 2,
-		text: "meeting with",
-		isCompleted: true
-	},
-	{
-		id: 3,
-		text: "appointment",
-		isCompleted: true
-	},
-	{
-		id: 3,
-		text: "appointment com todos",
-		isCompleted: true
-	},
-]
-console.log(todos) 
-const todoJSON = JSON.stringify(todos) // 
-console.log(todoJSON)
+// const todos = [
+// 	{
+// 		id: 1,
+// 		text: "take out the trash",
+// 		isCompleted: true
+// 	},
+// 	{
+// 		id: 2,
+// 		text: "meeting with",
+// 		isCompleted: true
+// 	},
+// 	{
+// 		id: 3,
+// 		text: "appointment",
+// 		isCompleted: true
+// 	},
+// 	{
+// 		id: 3,
+// 		text: "appointment com todos",
+// 		isCompleted: false
+// 	},
+// ]
+// console.log(todos)
+// const todoJSON = JSON.stringify(todos) //
+// console.log(todoJSON)
 
-// For
-for (let i = 0; i < todos.length; i++){
-	console.log(todos[i].text)
+// // For
+// for (let i = 0; i < todos.length; i++){
+// 	console.log(todos[i].text)
+// }
+
+// //While
+// let i = 0
+// while (i < 10 ) {
+// 	console.log(`while  loop number: ${i}`)
+// 	i++
+// }
+
+// //forEAch
+// todos.forEach(function(todo) {
+// 	console.log(todo.text)
+// })
+
+// //Map
+// const todoText = todos.map(function (todo) {
+//   return todo.text
+// });
+// console.log(todoText)
+
+// //Filter
+// const todoTextFil = todos.filter(function (todo) {
+//   return todo.isCompleted === true ;
+// }).map(function (todo) {
+// 	return todo.text;
+// })
+// console.log(todoTextFil);
+
+// //Condicionals
+// const x = 10
+// const y = 10
+
+// if (x === 10 || y < 10) {
+// 	console.log(`${x}`)
+// } else if (x < 10){
+// 	console.log(`x is mais que ${x}`)
+// } else {
+// 	console.log(` ${x} e menos que 10`)
+// }
+
+// const cor = "whitetheme"
+
+// const color = cor === "dark" ? "darktheme" : "whitetheme"
+
+// // console.log(color)
+
+// //Switches
+// switch (color) {
+// 	case "darktheme":
+// 		console.log("darktheme")
+// 		break
+// 	case "blueTheme":
+// 		console.log("whitetheme")
+// 		break
+// 	default:
+// 		console.log("no theme")
+// 		break
+// }
+
+// //Functions
+// function addNums(num1,num2) {
+// 	console.log(num1 + " " + num2)
+// }
+// addNums("luan", "gama")
+
+// // Arrow Functions
+// const addNums2 = (num1 = 1, num2 = 2) => num1 + num2
+// console.log(addNums2(5,5))
+
+//Constructor function
+function Pessoa(firstName, lastName,dob) {
+	this.firstName = firstName
+	this.lastName = lastName
+	this.dob = new Date(dob)
+	this.getBirthYear = function () {
+		return this.dob.getFullYear()
+	}
 }
 
-//While
-let i = 0 
-while (i < 10 ) {
-	console.log(`while  loop number: ${i}`)
-	i++
-}
+//instantiate object
+const person1 = new Pessoa("luan", "cerqueira", "13-08-1997")
+console.log(person1.getBirthYear())
