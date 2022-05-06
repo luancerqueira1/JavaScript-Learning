d3a804c6e096b5727adccacc00b50c558bfc6d1f
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-06 18:59:14 -0300

    Aula 7 Done

--- Freecodecamp.org/07-questions/setup/app.js
+++ Freecodecamp.org/07-questions/setup/app.js
@@ -1,2 +1,24 @@
 //using selectors inside the element
-// traversing the dom
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(function (question) {
+	const btn = question.querySelector('.question-btn')
+	btn.addEventListener('click', function () {
+		questions.forEach(function (item) {
+			if (item !== question) {
+				item.classList.remove('show-text')
+			}
+		})
+		question.classList.toggle('show-text')
+	})
+})
+/*-------------------------------------------------------------- */
+// // traversing the dom
+// const Btns = document.querySelectorAll('.question-btn')
+
+// Btns.forEach(function (btn) {
+// 	btn.addEventListener('click', function (e) {
+// 		const question = e.currentTarget.parentElement.parentElement
+// 		question.classList.toggle('show-text')
+// 	})
+// })
