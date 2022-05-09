f72fd82979b60e499fbe585b76f79a6ef4cf7ae7
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-09 13:12:36 -0300

    terminar aula 11

--- Freecodecamp.org/10-scroll/setup/app.js
+++ Freecodecamp.org/10-scroll/setup/app.js
@@ -23,6 +23,49 @@ navToggle.addEventListener('click', function () {
 	}
 })
 // ********** fixed navbar ************
+const navBar = document.querySelector('#nav')
+const topLink = document.querySelector('.top-link')
 
+window.addEventListener('scroll', function () {
+	const scrollHeight = window.pageYOffset
+	const navheight = navBar.getBoundingClientRect().height
+	if (scrollHeight > navheight) {
+		navBar.classList.add('fixed-nav')
+	}
+	else {
+		navBar.classList.remove('fixed-nav')
+	}
+	if (scrollHeight > 500){
+		topLink.classList.add('show-link')
+	} else {
+		topLink.classList.remove('show-link')
+	}
+})
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach(function (link) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault()
+		//Navigate to especific spot
+		const id = e.currentTarget.getAttribute('href').slice(1)
+		const element = document.getElementById(id)
+		// Calculate the heights
+		const navHeight = navBar.getBoundingClientRect().height
+		const containerHeight = linksContainer.getBoundingClientRect().height
+		const fixedNav = navBar.classList.contains('fixed-nav')
+		let position = element.offsetTop - navHeight
+		if (!fixedNav) {
+			position = position - navHeight
+		}
+		if (navHeight > 82) {
+			position = position + containerHeight
+		}
+		window.scrollTo({
+			left: 0,
+			top: position,
+		})
+		linksContainer.style.height = 0
+	})
+})
