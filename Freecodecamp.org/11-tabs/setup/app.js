d6df75426b94d7f33719a911c6c834e3f1f6bcaf
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-09 19:20:31 -0300

    done for now

--- Freecodecamp.org/11-tabs/setup/app.js
+++ Freecodecamp.org/11-tabs/setup/app.js
@@ -1,22 +1,25 @@
-
-const btns = document.querySelectorAll('.tab-btn');
 const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
 
-about.addEventListener('click', function (e) {
-	const id = e.target.dataset.id
-	if (id) {
-		// Remove atcive buttons
-		btns.forEach(function (btn) {
-			btn.classList.remove('acitve')
-			e.target.classList.add("active");
-			
-		})
-		//Hide other articles
-		articles.forEach(function (article) {
-			article.classList.remove('active')
-		})
-		const element = document.getElementById(id)
-		element.classList.add('active')
-	}
-})
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // Remove atcive buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    //Hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
+
+
+
+
--- Freecodecamp.org/12-countdown-timer/setup/app.js
+++ Freecodecamp.org/12-countdown-timer/setup/app.js
@@ -21,3 +21,19 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll('.deadline-format h4')
+
+let futureDate = new Date(2022, 4, 24, 11, 30, 0)
+
+const year = futureDate.getFullYear() 
+const hours = futureDate.getHours()
+const minuts = futureDate.getMinutes()
+
+let month = futureDate.getMonth()
+month = months[month]
+const date = futureDate.getDate()
+giveaway.textContent = `ends on ${date} ${month} ${year} ${hours}:${minuts}`
+
