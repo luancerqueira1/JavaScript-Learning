53d8eba907b0bfbbb7b6bf8f308843c0cd6376ee
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-12 17:45:06 -0300

    aula 15 done

---
+++ Freecodecamp.org/15-slider/setup/app.js
@@ -0,0 +1,44 @@
+const slides = document.querySelectorAll('.slide')
+const nextBtn = document.querySelector('.nextBtn')
+const prevbtn = document.querySelector('.prevBtn')
+
+slides.forEach(function (slide, index) {
+	slide.style.left = `${index * 100}%`
+})
+
+let counter = 0
+nextBtn.addEventListener('click', function () {
+	counter++
+	carousel()
+})
+prevbtn.addEventListener('click', function () {
+	counter--
+	carousel()
+})
+
+function carousel() {
+	//working with slides 
+	if (counter ===  slides.length) {
+		counter = 0
+	}
+	if (counter < 0) {
+		counter = slides.length - 1
+	}
+	if (counter < slides.length - 1) {
+		nextBtn.style.display ='block'
+	}
+	else {
+		nextBtn.style.display = 'none'
+	}
+	if (counter > 0) {
+		prevbtn.style.display = 'block'
+	}
+	else {
+		prevbtn.style.display = 'none'
+	}
+	slides.forEach(function (slide) {
+		slide.style.transform = `translateX(-${counter * 100}%)`
+	})
+}
+
+prevbtn.style.display = 'none'
