87edfc0658d35958eb2c9fdf46fbb498a27c4e20
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-17 12:35:35 -0300

    bosta ja deu erro

--- Florinpop10hoursproject/quizapp/app.js
+++ Florinpop10hoursproject/quizapp/app.js
@@ -33,19 +33,55 @@ const quizData = [
   }
 ];
 
-const questionEl = document.getElementById('question');
-const a_text =document.getElementById('a-text');
-const b_text =document.getElementById('b-text');
-const c_text =document.getElementById('c-text');
-const d_text =document.getElementById('d-text');
+const questionE1 = document.getElementById('question');
+const a_text = document.getElementById('a-text');
+const b_text = document.getElementById('b-text');
+const c_text = document.getElementById('c-text');
+const d_text = document.getElementById('d-text');
+const submitBtn = document.querySelector("#submitBtn"); 
 
-let currentQuiz= 0;
+let currentQuiz = 0;
+let score = 0
 
 loadQuiz();
 
 function loadQuiz() {
-	const currentQuizData = quizData[currentQuestion];
-	questionEl.innerHTML = currentQuizData.question;
+	const currentQuizData = quizData[currentQuiz];
+	questionE1.innerHTML = currentQuizData.question;
 
-
+  a_text.innerHTML = currentQuizData.a;
+  b_text.innerHTML = currentQuizData.b;
+  c_text.innerHTML = currentQuizData.c;
+  d_text.innerHTML = currentQuizData.d;
 }
+
+function getSelected() {
+  const answerEls = document.querySelectorAll(".answer");
+
+  let answer = undefined;
+
+  answerEls.forEach((answerE1) => {
+    if (answerE1.cheked) {
+      answer = answerE1.id
+    }
+  });
+  return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+  //check to see the answer 
+  const answer = getSelected();
+  console.log(answer);
+  
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++
+    }
+    if (currentQuiz < quizData.length) {
+      loadQuiz()
+    }
+    else {
+      alert('fimished')
+    }
+  }
+});
