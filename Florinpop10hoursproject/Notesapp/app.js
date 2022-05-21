8990d9db74848eed5d076febd05fa4d337f18ae7
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-21 16:06:10 -0300

    notes app trash desisti

--- Florinpop10hoursproject/Notesapp/app.js
+++ Florinpop10hoursproject/Notesapp/app.js
@@ -1,17 +1,52 @@
-
+const addBtn = document.querySelector(".add");
 const notesEl = document.querySelector(".notes");
-const editBtn = document.querySelector(".edit");
-const deleteBtn = document.querySelector(".delete");
 
-const main = notesEl.querySelector(".main");
-const textArea = notesEl.querySelector("textarea");
 
-editBtn.addEventListener("click", () => {
-  main.classList.toggle("hidden");
-  textArea.classList.toggle("hidden");
-});
+addBtn.addEventListener('click', () => {
+  addNewNote()
+})
+  
 
-textArea.addEventListener("input", (e) => {
-  const { value } = e.target;
-  main.innerHTML = marked(value);
-});
+
+function addNewNote() {
+  const note = document.createElement("div");
+  note.classList.add("note");
+
+  note.innerHTML = `
+   <div class="notes">
+    <div class="tools">
+      <button class="edit"><i class="fas fa-edit"></i></button>
+      <button class="delete"><i class="fas fa-trash"></i></button>
+    </div>
+    <div class="main hidden">
+    </div>
+     <textarea id="textarea"></textarea>
+   </div>
+   `;
+  
+  const editBtn = note.querySelector("edit");
+  const deleteBtn = note.querySelector(".delete");
+  
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+
+  addBtn.addEventListener("click", () => {
+    addNewNote();
+  });
+
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  deleteBtn.addEventListener('click', () => {
+    note.remove()
+  });
+
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+  });
+
+  document.body.appendChild(note);
+}
