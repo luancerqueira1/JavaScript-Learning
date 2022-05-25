33a53014b15c8db3ec7465c53a4e01a9f006753e
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-25 12:55:57 -0300

    designe quase pronto

--- Florinpop10hoursproject/Githubprofile/app.js
+++ Florinpop10hoursproject/Githubprofile/app.js
@@ -5,27 +5,31 @@ const form = document.getElementById('form');
 const main = document.getElementById('main');
 const search = document.getElementById("search");
 
-async function getUser() {
-	const resp = await fetch(APIURL + user);
-	const respData = await resp.json();
+getUser("luancerqueira1");
 
-	createUserCard(respData);
+async function getUser(username) {
+  const resp = await fetch(APIURL + username);
+  const respData = await resp.json();
+
+  createUserCard(respData);
 }
 
 function createUserCard(user) {
   const cardHTML = `
 	<div class="card">
-		<div>
+		<div class="avatar-container">
 			<img class="avatar"src="${user.avatar_url}" alt="${user.name}" />
 		</div>
-		<div>
+		<div class="user-info">
 			<h2>${user.name}</h2>
 			<p>${user.bio}</p>
 
 			<ul class="info">
-				<li>${user.followers}</li>
-				<li>${user.following}</li>
-				<li>${user.public_repos}</li>
+				<li>${user.followers}<strong>Folllowers </strong></li>
+				<li>${user.following}<strong>Folllowing </strong></li>
+				<li>${user.public_repos}<strong>Repositiry </strong></li>
+			</ul>
+			<ul id="repos">
 			</ul>
 		</div>
 	</div>	
