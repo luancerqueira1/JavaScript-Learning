71e9ca06d567dc5dd6d98ce08d600fef209a84b2
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-05-27 12:19:19 -0300

    generate app style done starting working on js

---
+++ Florinpop10hoursproject/password/app.js
@@ -0,0 +1,50 @@
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '_-.,';
+
+function getLowercase() {
+	return lowerLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+
+function getUppperCase() {
+	return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+	
+}
+
+
+function getNumber() {
+	return numbers[Math.floor(Math.random() * numbers.length)];
+	
+}
+
+
+function getSymbol() {
+	return symbols[Math.floor(Math.random() * symbols.length)];
+	
+}
+
+function generatePassword() {
+	const len = lenEl.value;
+
+	let password = '';
+
+	for (let i= 0; i < len; i++) {
+		const x = generateX();
+		password += x;
+	}
+	
+	pwEl.innerText = password;
+}
+
+generateEl.addEventListener('click', generatePassword);
