5e62bdc0061676e07436bf6e3f1b0c410b92e551
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-06-05 12:58:17 -0300

    portfolio ,projects , reviews Done

--- Responsive Portfolio Website Design Using HTML CSS & JavaScript - With Working Contact Form/js/app.js
+++ Responsive Portfolio Website Design Using HTML CSS & JavaScript - With Working Contact Form/js/app.js
@@ -3,3 +3,17 @@ const menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
 	menu.classList.toggle('move')
 }
+//revies swiper
+var swiper = new Swiper(".reviews-content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+	},
+  
+});
