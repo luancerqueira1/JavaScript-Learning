965863d9a1aea8314467e34416333b0a138ea78c
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-06-05 19:29:37 -0300

    design do footer a fazer

--- Responsive Portfolio Website Design Using HTML CSS & JavaScript - With Working Contact Form/js/app.js
+++ Responsive Portfolio Website Design Using HTML CSS & JavaScript - With Working Contact Form/js/app.js
@@ -17,3 +17,50 @@ var swiper = new Swiper(".reviews-content", {
 	},
   
 });
+
+// email js
+
+function validate() {
+  let name = document.querySelector('.name');
+  let email = document.querySelector('.email');
+  let msg = document.querySelector('.message');
+  let sendBtn = document.querySelector('.send-btn');
+  
+  sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    }
+    else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  })
+}
+
+validate();
+
+function sendmail(name,email,msg) {
+  emailjs.send("service_w2tj03t", "template_3jyk46p", {
+    to_name: name,
+    from_name: email,
+    message: msg,
+  });
+}
+
+function emptyerror() {
+  swal({
+    title: "Oh No...!",
+    text: "field cannot be empty!",
+    icon: "error",
+  });
+}
+
+function success() {
+  swal({
+    title: "Email sent Successfully!",
+    text: "we try to reply in 24 hours !",
+    icon: "success!",
+  });
+}
