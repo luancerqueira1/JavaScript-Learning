e0604f1bf72cf7ea5fb74de7f1d6b2eb2d5dca89
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-06-27 22:05:37 -0300

    aula funçcoes

--- rocketseatGuia Estelar JavaScript/app.js
+++ rocketseatGuia Estelar JavaScript/app.js
@@ -58,4 +58,55 @@
 	console.log(y);
 */
 
+//objsct
+// const person = {
+// 	name: 'luan',
+// 	age: 24,
+// 	height: 175,
+// 	weight : 70,
+// }
 
+// console.log(`meu nome é ${person.name}eu tenho ${person.age} anos`);
+
+//arrays
+// const animals = [
+// 	'lion',
+// 	'cat',
+// 	{
+// 		name: 'cat',
+// 		age: 2,
+// 	},
+// 	'dog'
+// ]
+// console.log(animals[2].name);
+
+// let weight;
+// console.log(typeof weight);
+
+// let name = 'luan';
+// let age = 24;
+// let stars = 4.4; // FLOAT - NUMERO QUEBRADO
+// let isSubscribed = true;
+
+/* FUNCTION */
+// const sum = function (a,b) { // funÇao anonima  -  A e B sao parametros
+// 	let total = a + b;
+// 	return total;
+// }
+// sum(2, 3) //2 e 3 sao argumentos
+// sum(3, 3)
+
+// let a = 20;
+// let b = 30;
+
+// console.log(`o primeiro numero é ${a}`);
+// console.log(`o segundo numero é ${b}`);
+// console.log(`a soma dos dois é ${sum(a, b)}`);
+
+/*--------------------------------- */
+function fazerSuco(fruta1 , fruta2) {
+	return fruta1 + fruta2;
+}
+
+const copo = fazerSuco('maracuja', ' manga')
+console.log(copo);
