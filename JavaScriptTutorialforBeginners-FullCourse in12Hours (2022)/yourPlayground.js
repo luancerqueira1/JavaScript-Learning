628e0735ef3174dabfa68f5f924669492cca7788
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-07-11 12:51:08 -0300

    8:40

--- JavaScriptTutorialforBeginners-FullCourse in12Hours (2022)/yourPlayground.js
+++ JavaScriptTutorialforBeginners-FullCourse in12Hours (2022)/yourPlayground.js
@@ -305,7 +305,7 @@
 
 /* SUPER HERO API */ // error
 
-// API URL 
+// API URL
 // const APIHERO = 'https://superheroapi.com/api.php/1481962918922066';
 
 // async function getRandomSuperHero(url) {
@@ -328,7 +328,7 @@
 // searchHeroBtn.onclick = () => searched(searchHero.value);
 // ;
 
-// //show hero picture 
+// //show hero picture
 // const getData = (show) => {
 //    const heroimg = document.querySelector('#hero');
 //    heroimg.setAttribute('src',`${show}`);
@@ -345,4 +345,126 @@
 //    const numberHeros = 371;
 //    return Number(Math.floor(Math.random() * numberHeros) + 1);
 // };
-// batman
+
+/* ASYNCHRONOUS */
+// const waitting = () => console.log('oi');
+// setTimeout(waitting, 2000);
+
+// const promise1 = new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//       isReady = false;
+//       isReady ? resolve('soup is ready') : reject('soup is not ready')
+//    },2000)
+// })
+// console.log(promise1
+//    .then(value => console.log(value))
+//    .catch(error => console.log({ error })));
+
+/* ASYNC AWAIT */
+// const APIURL = 'https://dog.ceo/api/breeds/image/random';
+
+// const getDog = async () => {
+//    try{
+//       const resp = await fetch(APIURL);
+//       const respData = await resp.json();
+//       console.log(respData);
+//    } catch {
+//       console.log('err');
+//    }
+// }
+// getDog();
+
+/* WEATHER APP */
+// const options = {
+//    method: 'GET',
+//    headers: {
+//       'X-RapidAPI-Key': '089ce5e3f3mshda14881f713c1a6p14cd3djsn2242b7b5dda0',
+//       'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+//    },
+// };
+
+// fetch(
+//    'https://community-open-weather-map.p.rapidapi.com/weather?q=chicago&units=imperial',
+//    options
+// )
+//    .then((response) => response.json())
+//    .then((response) => console.log(response))
+//    .catch((err) => console.error(err));
+
+// const API =
+//    'https://community-open-weather-map.p.rapidapi.com/weather?q=chicago&units=imperial';
+
+// async function getWeatherData() {
+//    const resp = await fetch(API, options);
+//    const respData = await resp.json();
+//    console.log(respData);
+// }
+
+// getWeatherData();
+
+/**
+ * Weather App
+ * TODO: Complete getWeatherData() to return json response Promise
+ * TODO: Complete searchCity() to get user input and get data using getWeatherData()
+ * TODO: Complete showWeatherData() to set the data in the the html file from response
+ */
+
+/* DIV ID's you'll need access to 👇
+"city-name"
+"weather-type"
+"temp"
+"min-temp"
+"max-temp"
+*/
+
+// API_KEY for maps api
+let API_KEY = 'a8e71c9932b20c4ceb0aed183e6a83bb';
+
+/**
+ * Retrieve weather data from openweathermap
+ * HINT: Use fetch()
+ * HINT: URL should look like this:
+ * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
+ */
+const getWeatherData = async (city) => {
+   const URL = 'https://api.openweathermap.org/data/2.5/weather';
+   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+   //HINT: Use template literals to create a url with input and an API key
+
+   //CODE GOES HERE
+   const resp = await fetch(FULL_URL);
+   const respData = await resp.json();
+   console.log(respData);
+
+   showWeatherData(respData);
+};
+/**
+ * Retrieve city input and get the weather data
+ * HINT: Use the promise returned from getWeatherData()
+ */
+const searchCity = () => {
+   const city = document.getElementById('city-input').value;
+   // CODE GOES HERE
+   getWeatherData('euclides da cunha');
+   
+};
+/**
+ * Show the weather data in HTML
+ * HINT: make sure to console log the weatherData to see how the data looks like
+ */
+const showWeatherData = (weatherData) => {
+   //CODE GOES HERE
+   const cityName = document.querySelector('#city-name');
+   const weatherType = document.querySelector('#weather-type');
+   const temp = document.querySelector('#temp');
+   const minTemp = document.querySelector('#min-temp');
+   const maxTemp = document.querySelector('#max-temp');
+
+   cityName.innerText = weatherData.name;
+   weatherType.innerText = weatherData.weather[0].main;
+   temp.innerText = weatherData.main.temp;
+   
+   minTemp.innerText = weatherData.main.temp_max;
+   maxTemp.innerText = weatherData.main.temp_min;
+   
+};
