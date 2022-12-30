ffb0d96d72209b7ec9f374b99efb86d320e79570
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-12-30 11:35:38 -0300

    math operators

---
+++ javascript-in-depth/math-operators/mathOperators.js
@@ -0,0 +1,25 @@
+ // addition
+let age = 10 + 10;
+console.log(age);
+ 
+//subtraction
+let points = 50 - 30;
+console.log(points);
+
+// multiplication 
+let score = 2 * 10;
+console.log(score);
+
+// division
+let divide = 100 / 10;
+console.log(divide);
+
+// module
+let mod = 9 % 3;
+console.log(mod);
+
+
+//exponentiation
+let exp = 4 ** 3;
+console.log(exp);
+
