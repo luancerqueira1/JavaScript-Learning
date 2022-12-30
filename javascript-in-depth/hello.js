db2c2000172a04913e3467f510fbf01168c07189
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-12-30 10:15:34 -0300

    primitives datatypes

--- javascript-in-depth/hello.js
+++ javascript-in-depth/hello.js
@@ -1,2 +1,3 @@
 console.log('oi');
 console.log('luan');
+
---
+++ javascript-in-depth/primitives/primitives.js
@@ -0,0 +1,29 @@
+//strings
+
+let city = 'Toronto';
+
+city = ' canda';
+
+city = 'india';
+
+const name = 'luan';
+
+//number
+
+const age = 35;
+
+let price = 1.23;
+
+
+//boolean
+
+const happy = false;
+
+let sad = true;
+
+// null &  undefined
+
+const empty = null;
+
+const empty2 = undefined;
+
