048036ad95f4356ee7d6d5a7780d6f3989ad067d
Luan Cerqueira <luangamacerqueira1@gmail.com>
2023-01-05 12:23:03 -0300

    boolean operators

---
+++ javascript-in-depth/boolean-operators/boollean.js
@@ -0,0 +1,68 @@
+//strict quality
+
+console.log(5 === 5);
+
+console.log('------------');
+
+
+// strict not equal
+
+console.log(5 !== 5);
+
+console.log('------------');
+
+
+// boolean mathematical operators
+
+console.log( 9 > 7);
+console.log( 5 > 5);
+console.log( 5 < 5);
+console.log(5 < 1);
+
+console.log('------------');
+
+// and operators
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+console.log('------------');
+
+console.log(true || true);
+console.log(true || false);
+console.log(false|| false);
+
+console.log('------------');
+
+// falsy values
+
+console.log(false);
+console.log(0);
+console.log('');
+console.log(null);
+console.log(undefined);
+console.log(NaN);
+
+console.log('------------');
+
+
+// truthy values
+
+
+// logical assignment
+
+let teste1 = true && 'luan';
+console.log(teste1);
+
+let teste2 = false && 'luan';
+console.log(teste2);
+
+let teste3 = true || 'bananas';
+console.log(teste3);
+
+let teste4 = false || 'luan';
+console.log(teste4);
+
+
+
