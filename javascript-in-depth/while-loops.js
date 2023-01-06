6a5195a3c556a47f3f6f76591fafb3554fbae62e
Luan Cerqueira <luangamacerqueira1@gmail.com>
2023-01-06 12:14:57 -0300

    loops

---
+++ javascript-in-depth/while-loops.js
@@ -0,0 +1,18 @@
+let greeting = 0;
+
+while (greeting < 4) {
+   console.log('oi');
+   greeting++
+}
+
+let score = 0
+while (true) {
+   console.log('inside loop');
+
+   if (score >= 3) {
+      break;
+   }
+
+   score++;
+}
+
