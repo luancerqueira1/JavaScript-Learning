1fd5afc5895b5b6652c502c60d32a561fd592275
Luan Cerqueira <luangamacerqueira1@gmail.com>
2023-01-07 12:08:50 -0300

    for loops

---
+++ javascript-in-depth/for-loops.js
@@ -0,0 +1,16 @@
+//create a for loop that prints out the numbers from 5 to 10
+
+for (let i = 5; i <= 10; i++){
+   console.log(i);
+}
+
+//print even numbers between 10 and 40
+
+for (let num = 10; num <= 40; num++){
+   if (num % 2 === 0) {
+      console.log(num, 'even');
+   }
+   if (num % 2 === 1) {
+      console.log(num ,'odd');
+   }
+}
