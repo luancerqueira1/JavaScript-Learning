246b7f516eafd87eacef9c5d37404d8dfa06f94d
Luan Cerqueira <luangamacerqueira1@gmail.com>
2023-01-07 10:56:37 -0300

    while loops

--- javascript-in-depth/while-loops.js
+++ javascript-in-depth/while-loops.js
@@ -2,10 +2,10 @@ let greeting = 0;
 
 while (greeting < 4) {
    console.log('oi');
-   greeting++
+   greeting++;
 }
 
-let score = 0
+let score = 0;
 while (true) {
    console.log('inside loop');
 
@@ -16,3 +16,34 @@ while (true) {
    score++;
 }
 
+// criar loop that will print out all the even numbers
+
+let count = 10;
+while (count <= 40) {
+   if (count % 2 === 0) {
+      console.log(count, 'even');
+   } else {
+      console.log(count, 'odd');
+   }
+   count++;
+}
+
+let contador = 1;
+
+while (contador <= 100) {
+   if (contador === 50) {
+      console.log('half way there');
+      contador++;
+      continue;
+   }
+   if (contador === 100) {
+      console.log('you made it');
+      contador++;
+      continue;
+   }
+
+   if (contador % 10 === 0) {
+      console.log('checkpoint', contador);
+   }
+   contador++;
+}
