218285c14230f43bb54070e25a4da4d4a7683484
Luan Cerqueira <luangamacerqueira1@gmail.com>
2023-01-30 12:21:14 -0300

    reduce

--- javascript-in-depth/reduce.js
+++ javascript-in-depth/reduce.js
@@ -9,9 +9,9 @@ const localidade = citys.reduce((result, city) => {
 console.log('result',localidade);
 
 
-const sum = [5, 10, 15, 20, 25, 30, 35];
+const sum1 = [5, 10, 15, 20, 25, 30, 35];
 
-const sum2 = sum.reduce((result, number) => {
+const sum2 = sum1.reduce((result, number) => {
    if (result < 20) {
       return result + number
    }
@@ -33,3 +33,85 @@ const concatCities = cities.reduce((concatString, city) => {
 },'');
 
 console.log(concatCities);
+console.log('------------------');
+
+const points = [55, 56, 57, 58, 59, 60];
+
+const sum = points.reduce((result, point) => {
+   return result + point
+},0);
+
+console.log(sum);
+
+console.log('------------------');
+
+const companies = ['apple', 'spaceX', 'tesla', 'meta', 'google','luan'];
+
+const modded = companies.reduce((result, company,index) => {
+   if (company.startsWith('a')) {
+      return result 
+   }
+
+   if (index === companies.length -1) {
+      return result + company;
+   }
+   return result + company + '-';
+
+}, '')
+
+console.log(modded);
+
+console.log('------------------');
+
+
+const prices = [1.23, 19.99, 85.2, 32.87, 8, 5.2];
+
+const afterTax = prices.reduce((sum, price) => {
+   if (price > 6) {
+      return sum + price;
+   }
+
+   return sum + price * 1.2
+}, 0);
+
+console.log(afterTax);
+
+console.log('------------------');
+
+
+const itens = ['light', 'banana', 'phone', 'mouse'];
+
+const caps = itens.map((item) =>{
+   return item.toUpperCase()
+})
+
+const concat = caps.reduce((result, string) => {
+   return result + string + ' '
+}, '');
+
+console.log(concat);
+
+console.log('------------------');
+
+
+
+const nums = [10, 30, 50, 70, 90];
+
+const squares = nums.map((num) => {
+   return num ** 2;
+})
+
+const over1000 = squares.filter((num)=> {
+   if (num > 1000) {
+      return true;
+   }
+   return false
+})
+
+const finale = over1000.reduce((result,num) => {
+   return  result +  num
+}, 0)
+
+console.log(squares);
+console.log(over1000);
+console.log(finale);
